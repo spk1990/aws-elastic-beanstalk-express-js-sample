8a4fe8d7348ab9a22886ef2c099fe4568e92e431
spk1990 <101204220+spk1990@users.noreply.github.com>
2022-05-25 20:12:34 +0100

    class test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World test 2!'));
+app.get('/', (req, res) => res.send('Hello class!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
